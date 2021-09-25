018619b326cd69e18fcacb022a52512a6bf92b66
majed2013 <47279932+majed2013@users.noreply.github.com>
2021-09-25 20:38:38 +0300

    Create Watermelon_solution.cpp

---
+++ Watermelon_solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+    int x;
+    cin>>x;
+
+    if (x % 2==0 && x>2){
+            cout<<"yes";
+
+    }
+    else
+        cout<<"NO";
+
+
+
+
+    return 0;
+}
